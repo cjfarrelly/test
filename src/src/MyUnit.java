fc5585984347e60ebc87c1ae50a49c889da088b5
Colin Farrelly <cjfarrelly@gmail.com>
2018-12-05 00:05:12 +0000

    main and print line

--- src/src/MyUnit.java
+++ src/src/MyUnit.java
@@ -1,5 +1,10 @@
 
 public class MyUnit {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello there sunny jim.");
+	}
+	
     public String concatenate(String one, String two){
         return one + two;
     }
